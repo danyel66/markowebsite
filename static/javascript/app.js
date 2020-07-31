4462a90692c36a0546f5bcfc1da5d537bb2bee67
Somto <ikwuomzy@gmail.com>
2020-07-31 12:35:10 -0400

    Fortify all web pages

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,36 +1,46 @@
 var category = document.getElementsByName('category');
 
-category[0].onclick = function(){
-    category[0].classList.add('active');
-    category[1].firstElementChild.setAttribute("src", "../images/goat1.png");
-    category[0].firstElementChild.setAttribute("src", "../images/growth1.png");
+category[0].onclick = function() {
 
+    category[0].firstElementChild.setAttribute("src", "/static/images/growth1.png");
+    category[1].firstElementChild.setAttribute("src", "/static/images/cow1.png");
+    category[2].firstElementChild.setAttribute("src", "/static/images/services-0.png");
+
+    category[0].classList.add('active');
     category[1].classList.remove('active');
+    category[2].classList.remove('active');
 
     var prodCate = document.querySelector(".product-category").style.left ="0";
 }
 
-category[1].onclick = function(){
+category[1].onclick = function() {
+    category[1].firstElementChild.setAttribute("src", "/static/images/cow2.png");
+    category[0].firstElementChild.setAttribute("src", "/static/images/growth2.png");
+    category[2].firstElementChild.setAttribute("src", "/static/images/services-0.png")
+
     category[1].classList.add('active');
-    category[1].firstElementChild.setAttribute("src", "../images/goat2.png");
-    category[0].firstElementChild.setAttribute("src", "../images/growth2.png");
     category[0].classList.remove('active');
-    category[2].firstElementChild.setAttribute("src", "../images/services-0.png");
     category[2].classList.remove('active');
+
     var prodCate = document.querySelector(".product-category").style.left ="-100%";
 
 }
 
-category[2].onclick = function(){
-    category[1].classList.remove('active');
-    category[1].firstElementChild.setAttribute("src", "../images/goat1.png");
-    category[0].firstElementChild.setAttribute("src", "../images/growth2.png");
-    category[0].classList.remove('active');
-    category[2].firstElementChild.setAttribute("src", "../images/services-1.png");
-    category[2].classList.add('active');
-    var prodCate = document.querySelector(".product-category").style.left ="-200%";
+category[2].onclick = function() {
+  category[2].firstElementChild.setAttribute("src", "/static/images/services-1.png")
+  category[0].firstElementChild.setAttribute("src", "/static/images/growth2.png");
+  category[1].firstElementChild.setAttribute("src", "/static/images/cow1.png");
+  
+  
+
+  category[2].classList.add('active');
+  category[0].classList.remove('active');
+  category[1].classList.remove('active');
+
+  var prodCate = document.querySelector(".product-category").style.left ="-200%";
 
 }
+
 
 var pagination = document.getElementsByName("pag2");
 var pta = document.getElementsByName("pta");
